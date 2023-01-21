700b9df2191e070d074f68081152b936879a7d01
simon <simon.weckler@mnet-online.de>
2023-01-21 15:16:18 +0100

    feat: fuck this

--- public/index.php
+++ public/index.php
@@ -3,7 +3,10 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Views\Twig;
 use Slim\Views\TwigMiddleware;
+use Slim\Routing\RouteContext;
 use Slim\Factory\AppFactory;
+
+require 'src/Product.php';
 
 require __DIR__ . '/../vendor/autoload.php';
 
@@ -23,9 +26,21 @@ $app->get('/', function (Request $request, Response $response, $args) {
     ]);
 });
 
-$app->get('/shopping', function (Request $request, Response $response, $args) {
+$app->get('/search', function (Request $request, Response $response, $args) {
     $view = Twig::fromRequest($request);
-    return $view->render($response, 'shopping.html');
+    
+    $requestParams = $request->getQueryParams();
+    $searchParam = $requestParams['q'];
+
+    $products = [];
+
+    if($searchParam != NULL){
+        $products = [new Product("hi", "hi", "https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cHJvZHVjdHxlbnwwfHwwfHw%3D&w=1000&q=80")];
+    }
+
+    return $view->render($response, 'shopping.html', [
+        'products' => $products
+    ]);
 });
 
 $app->get('/martin', function (Request $request, Response $response, $args) {
