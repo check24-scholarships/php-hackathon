d984db6e45505df3f8b260b26e6481766ddf2853
simon <simon.weckler@mnet-online.de>
2023-01-21 15:28:21 +0100

    fix: asc

--- public/index.php
+++ public/index.php
@@ -6,7 +6,7 @@ use Slim\Views\TwigMiddleware;
 use Slim\Routing\RouteContext;
 use Slim\Factory\AppFactory;
 
-require 'src/Product.php';
+include 'src/databaseFunctions.php';
 
 require __DIR__ . '/../vendor/autoload.php';
 
@@ -30,12 +30,17 @@ $app->get('/search', function (Request $request, Response $response, $args) {
     $view = Twig::fromRequest($request);
 
     $requestParams = $request->getQueryParams();
-    $searchParam = $requestParams['q'];
-
     $products = [];
 
-    if($searchParam != NULL){
-        $products = [new Product("hi", "hi", "https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cHJvZHVjdHxlbnwwfHwwfHw%3D&w=1000&q=80")];
+    $db = new DBFuncs('db.sqlite3');
+    $sort = 'DESC';
+
+    if(array_key_exists('sort') && strtolower($requestParams['sort']) == "asc"){
+        $sort = 'ASC';
+    }
+
+    if(array_key_exists('q', $requestParams)){
+        $products = $db->searchProductByName($requestParams['q'], $sort);
     }
 
     return $view->render($response, 'shopping.html', [
